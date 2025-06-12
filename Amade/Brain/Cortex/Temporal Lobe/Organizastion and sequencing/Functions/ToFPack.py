bac8062c8efcf6edc57c1775732d5e9b092ac6b1
zwarriorxz <kluzab25@phahs.org.za>
2025-06-12 09:38:06 +0000

    emo-folder-names-update.1

--- Amade/Brain/Cortex/Temporal Lobe/Organizastion and sequencing/Functions/ToFPack.py
+++
@@ -1,7 +0,0 @@
-def ToF(str):
-    if str.lower() == 't':
-        return True
-    elif str.lower() == 'f':
-        return False
-    else:
-        raise ValueError("Not a valid argument: " + str)
